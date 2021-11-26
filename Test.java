88baaa495684cf178b12fdaf158ffb679ded5ec9
Siddhartha <siddharthapaul101@gmail.com>
2021-11-26 16:14:03 +0530

    Created Student and Test class

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student(101, "Siddhartha", "Ranchi");
+
+        System.out.println(student);
+    }
+}
